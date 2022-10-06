6aeeed718165791113d6bff4569095a8edf095ac
Evgeny <deevzhenja@inbox.ru>
2022-10-06 16:37:29 +0300

    Сделал задание 38

--- Example_38/Program.cs
+++ Example_38/Program.cs
@@ -7,38 +7,34 @@ Console.Clear();
 void InputArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next(1, 101);
+    array[i] = new Random().Next(1, 100);
 }
 
-int MaxNumbers(int[] array)
+int difference(int[] array)
 {
-    int max = 1;
-    for (int i = 2; i < array.Length; i++)
-    {
-        if(array[i] > array[max])
-         max = i;
-         return max;
-    }
-}
+    int dif = 0;
+    int max = 0;
+    int min = array[0];
 
-int MinNumbers(int[] array)
-{
-    int min = 1;
-    for (int i = 2; i < array.Length; i++)
-    {
-        if(array[i] < array[min])
-         min = i;
-         return min;
-    }
-}
 
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > max)
+            max = array[i];
+
+    if (array[i] < min)
+        {
+            min = array[i];
+        }
+
+        dif = max - min;
+
+    }
+    return dif;
+}
 Console.WriteLine("Введите колличество элементов в массиве: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[n];
 InputArray(array);
 Console.WriteLine("[" + string.Join(",", array) + "]");
-//Console.WriteLine($"Разницу между максимальным и минимальным элементом массива будет равна: {difference}");
-
- /*write(array[max], "-", array[min], "=", array[max] - array[min]);
- if (array[i] < array[min])
-         min = i; */
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива: {difference(array)}");
